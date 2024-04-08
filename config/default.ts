72faa57b43e8c4390be32983dcfb83b4ac5836f3
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2024-04-08 12:39:28 -0300

    enviando pastas

---
+++ config/default.ts
@@ -0,0 +1,11 @@
+export default {
+    port: 1337,
+    dbUri: "mongodb+srv://HelenaLannes:Hls270502he@cluster1.z9y1vyc.mongodb.net/testeback?retryWrites=true&w=majority",
+    saltWorkFactor: 10,
+    accessTokenTtl: "15m",
+    refreshTokenTtl: "1y",
+    accessTokenPrivateKey: ``,
+    accessTokenPublicKey: ``,
+    refreshTokenPrivateKey: ``,
+    refreshTokenPublicKey: ``,
+  };
